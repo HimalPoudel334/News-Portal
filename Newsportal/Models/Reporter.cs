3b6a296348afebc0518c542877e3043cd43175c1
Himal Poudel <himalpoudel334@gmail.com>
2022-05-15 10:19:59 +0545

    Added display name annotations

--- Newsportal/Models/Reporter.cs
+++ Newsportal/Models/Reporter.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Newsportal.Models
 {
     public class Reporter : IdentityUser
     {
         [Required]
+        [Display(Name = "LicenseNumber")]
         public string LisenceNumber { get; set; }
+        
         [Required]
-
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        
         [Required]
-
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        
         [Required]
         public string Address { get; set; }
     }
